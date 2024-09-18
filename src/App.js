c3569aff8ff4fe1ead5c4183e83e3d996a911f5c
rudotino <rtinofireyi@gmail.com>
2024-09-18 19:21:01 +0200

    Added GitHub and Netfly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"> WeatherApp </div>;
+  return (
+    <div className="App">
+      {" "}
+      <h1> WeatherApp </h1>
+      <footer>
+        This project was coded by Rudo Tino and is{" "}
+        <a
+          href="https://github.com/rudotino/rudo-react-weather"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;

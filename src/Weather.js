8151d21c01500e847e3b0a5af2adbf84a1cb0b26
rudotino <rtinofireyi@gmail.com>
2024-09-26 20:18:18 +0200

    Updated Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -7,14 +7,15 @@ export default function Weather(props) {
 
   function handleResponse(response) {
     setWeatherData({
-      ready: true,
-      temperature: response.data.main.temp,
-      humidity: response.data.main.humidity,
+      temperature: response.data.temperature.current,
+      //humidity: response.data.main.humidity,
       date: "Thursday 13:00",
-      description: response.data.weather[0].description,
-      iconURL: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      description: response.data.condition.description,
+      iconUrl: response.data.condition.icon_url,
+      // iconURL: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
       wind: response.data.wind.speed,
-      city: response.data.name,
+      city: response.data.city,
+      ready: true,
     });
   }
 
@@ -42,13 +43,17 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.time}</li>
-          <li>{weatherData.description}</li>
+          <li>{weatherData.date}</li>
+          <li classname="text capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
           <div className="col-6">
             <div className="clearfix">
-              <img src={weatherData.iconUrl} alt={weatherData.description} />
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                classname="float-left"
+              />
               <div>
                 <span className="temperature">
                   {Math.round(weatherData.temperature)}
@@ -59,7 +64,6 @@ export default function Weather(props) {
           </div>
           <div className="col-6">
             <ul>
-              <li>Precipitation: 0%</li>
               <li>Humidity: {weatherData.humidity}%</li>
               <li>Wind: {weatherData.wind}km/h</li>
             </ul>

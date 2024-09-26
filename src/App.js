effbd56c44e22335a64e2cb2bda71b1fea20f26f
rudotino <rtinofireyi@gmail.com>
2024-09-26 13:37:29 +0200

    Added bootstrap from npm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       {" "}
-      <h1> WeatherApp </h1>
-      <h2>Hello weather</h2>
-      <button className="btn btn-primary">Hello</button>
-      <footer>
-        This project was coded by Rudo Tino and is{" "}
-        <a
-          href="https://github.com/rudotino/rudo-react-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> WeatherApp </h1>
+        <h2>Hello weather</h2>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          This project was coded by Rudo Tino and is{" "}
+          <a
+            href="https://github.com/rudotino/rudo-react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

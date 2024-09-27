074d31dfec3c6ea582aac41129da0e4cf0a18578
rudotino <rtinofireyi@gmail.com>
2024-09-27 11:47:21 +0200

    Updated Weather CSS

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -9,7 +9,7 @@ export default function WeatherInfo(props) {
         <li>
           <FormattedDate date={props.data.date} />
         </li>
-        <li classname="text capitalize">{props.data.description}</li>
+        <li className="text capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
         <div className="col-6">
@@ -19,15 +19,14 @@ export default function WeatherInfo(props) {
               alt={props.data.description}
               className="float-left"
             />
-            <div>
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°C</span>
-            </div>
+
+            <span className="temperature">
+              {Math.round(props.data.temperature)}
+            </span>
+            <span className="unit">°C</span>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-6 mt-3">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind}km/h</li>

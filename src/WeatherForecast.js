fe62611e6e514cb71e651446d6cbd0fa9d9d459a
rudotino <rtinofireyi@gmail.com>
2024-10-04 17:53:31 +0200

    Added Weather Forecast api with longitude & latitude

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,20 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "92fac6943a36d8fo46cf4t280d40b3de";
+  let longitude = props.coordinates.longitude;
+  let latitude = props.coordinates.latitude;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
